36b4b7756a742622b2e76e967a13a571c58a4799
rafael <rafaelerasmo6@gmail.com>
2018-01-13 19:28:03 -0600

    fix url

--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from '../../../images/logo.png';
+// import logo from '../../../images/logo.png';
+// <img src={logo} width={250} >
 import './related.css'
 
 function Related(props) {
 	return(
 		<div className="Related">
-			<img src={logo} width={250} />
+			<img src="images/logo.png" width={250} />
 		</div>
 	)
 }
