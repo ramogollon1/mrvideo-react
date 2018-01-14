38fb6955626e62863126a31bb70ce96cfb493b4f
Rafael Mogollón <ramogollon1@ujap.edu.ve>
2018-01-13 19:37:03 -0600

    Update related.js

--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from '../../../images/logo.png';
+// import logo from '../../../images/logo.png';
+// <img src={logo} width={250} >
 import './related.css'
 
 function Related(props) {
 	return(
 		<div className="Related">
-			<img src={logo} width={250} />
+			<img src="./dist/logo.c06f5e5a2ae47006c0b77be4de6a2077.png" width={250} />
 		</div>
 	)
 }
