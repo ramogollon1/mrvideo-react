74e2fcfbc5da907627ccdd5bb53c7f20290da695
rafael <rafaelerasmo6@gmail.com>
2018-01-13 19:41:41 -0600

    fix image

--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -6,7 +6,7 @@ import './related.css'
 function Related(props) {
 	return(
 		<div className="Related">
-			<img src="../../../dist/logo.c06f5e5a2ae47006c0b77be4de6a2077.png" width={250} />
+			<img src="images/logo.png" width={250} />
 		</div>
 	)
 }
